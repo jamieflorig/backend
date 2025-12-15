14a18b321a9955c783aa473d2732ca6fcbad8a20
jamieflorig <jamie.florig@msunaawan.edu.ph>
2025-12-16 02:46:44 +0500

    Video 7-Make it possible to connect - Florig, Jamie D.

--- djangobnb_backend/chat/api.py
+++ djangobnb_backend/chat/api.py
@@ -1,16 +1,28 @@
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.decorators import api_view
+
+# from rest_framework_simplejwt.authentication import JWTAuthentication
+# from rest_framework.permissions import IsAuthenticated
 
 from .models import Conversation
-from .serializers import ConversationListSerializer
+from .serializers import ConversationListSerializer, ConversationDetailSerializer
 
 @api_view(['GET'])
-@authentication_classes([JWTAuthentication])
-@permission_classes([IsAuthenticated])
+# @authentication_classes([JWTAuthentication])
+# @permission_classes([IsAuthenticated])
 def conversations_list(request):
-    # Get conversations where the current user is a participant
-    conversations = request.user.conversations.all()
-    serializer = ConversationListSerializer(conversations, many=True)
+    serializer = ConversationListSerializer(request.user.conversations.all(), many=True)
+
     return JsonResponse(serializer.data, safe=False)
+
+
+@api_view(['GET'])
+def conversations_detail(request, pk):
+    conversation = request.user.conversations.get(pk=pk)
+
+    conversation_serializer = ConversationDetailSerializer(conversation, many=False)
+
+    return JsonResponse({
+        'conversation': conversation_serializer.data
+    }, safe=False)
--- djangobnb_backend/chat/serializers.py
+++ djangobnb_backend/chat/serializers.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
-from .models import Conversation
+
+from .models import Conversation, ConversationMessage
+
 from useraccount.serializers import UserDetailSerializer
+
 
 class ConversationListSerializer(serializers.ModelSerializer):
     users = UserDetailSerializer(many=True, read_only=True)
 
     class Meta:
         model = Conversation
-        fields = ('id', 'users', 'updated_at')
+        fields = ('id', 'users', 'updated_at',)
+
+
+    
+class ConversationDetailSerializer(serializers.ModelSerializer):
+    users = UserDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Conversation
+        fields = ('id', 'users', 'updated_at',)
