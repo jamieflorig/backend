d66736917ce868d905698ef490fa011f306f4f0f
jamieflorig <jamie.florig@msunaawan.edu.ph>
2025-12-16 13:57:44 +0500

    Video 7-Start new conversation - Florig, Jamie D.

--- djangobnb_backend/chat/api.py
+++ djangobnb_backend/chat/api.py
@@ -8,6 +8,8 @@ from rest_framework.decorators import api_view
 from .models import Conversation, ConversationMessage
 from .serializers import ConversationListSerializer, ConversationDetailSerializer, ConversationMessage, ConversationMessageSerializer
 
+from useraccount.models import User
+
 @api_view(['GET'])
 # @authentication_classes([JWTAuthentication])
 # @permission_classes([IsAuthenticated])
@@ -28,3 +30,19 @@ def conversations_detail(request, pk):
         'conversation': conversation_serializer.data,
         'messages': messages_serializer.data
     }, safe=False)
+
+@api_view(['GET'])
+def conversations_start(request, user_id):
+    conversations = Conversation.objects.filter(users__in=[user_id]).filter(users__in=[request.user.id])
+
+    if conversations.count() > 0:
+        conversation = conversations.first()
+
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
+    else:
+        user = User.objects.get(pk=user_id)
+        conversation = Conversation.objects.create()
+        conversation.users.add(request.user)
+        conversation.users.add(user)
+
+        return JsonResponse({'success': True, 'conversation_id': conversation.id})
